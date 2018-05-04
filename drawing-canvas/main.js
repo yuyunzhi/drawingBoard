270926fccad4bd18c0333c1175723ea453099209
e <lambert1129@foxmail.com>
2018-05-04 23:37:53 +0800

    support移动页面

--- drawing-canvas/main.js
+++ drawing-canvas/main.js
@@ -1,4 +1,35 @@
 
+var canvas=document.getElementById('canvas');
+var context=canvas.getContext('2d');
+
+var using=false;
+var lastPoint={
+    x:undefined,
+    y:undefined
+}
+
+/*画板逻辑 */
+autoSetSize(canvas);
+
+listenToUser(canvas);
+
+
+/********/
+  
+var eraserEnabled=false;
+
+eraser.onclick=function(){
+    eraserEnabled = !eraserEnabled;
+    if(eraserEnabled){
+        eraser.textContent= "画笔";
+    }else{
+        eraser.textContent= "橡皮擦";
+    }
+}
+
+/********/
+
+
 //drawLine
 function drawLine(x1,y1,x2,y2){
     context.beginPath();
@@ -19,80 +50,97 @@ context.fill();
 }
 
 //重置canvas画板宽高
-function setCanvasSize(yyy){
+function setCanvasSize(){
     var pageWidth=document.documentElement.clientWidth;
     var pageHeight=document.documentElement.clientHeight;
    
-    yyy.width = pageWidth;
-    yyy.height = pageHeight;
+    canvas.width = pageWidth;
+    canvas.height = pageHeight;
 
  }
 
-
-/********/
-
-
-var yyy=document.getElementById('canvas');
-var context=yyy.getContext('2d');
-
-var using=false;
-var lastPoint={
-    x:undefined,
-    y:undefined
+//自动设置canvas画板宽高
+function autoSetSize(){
+    setCanvasSize();    
+    window.onresize =function(){
+        setCanvasSize();
+    }    
 }
 
-setCanvasSize(yyy);
 
-window.onresize =function(){
+function listenToUser(){
+    //特性检测
+    if(document.body.ontouchstart!== undefined ){                    
+        //是触屏设备
+        //
+        canvas.ontouchstart =function(aaa){
+            var x=aaa.touches[0].clientX;
+            var y=aaa.touches[0].clientY;
+            using=true;
+            lastPoint={x:x,y:y};
+            if(eraserEnabled){
+                context.clearRect(x-10,y-10,20,20);
+            }else{
+                drawCir(x,y);
+            }
+        }
+        //
+        canvas.ontouchmove = function(aaa){
+            var x=aaa.touches[0].clientX;
+            var y=aaa.touches[0].clientY;
+            var newPoint={x:x,y:y}
+            if(using){
+                if(eraserEnabled){
+                    context.clearRect(x-10,y-10,20,20);
+              }else{
+                    drawCir(x,y);
+                    drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+                    lastPoint=newPoint;      
+                }
+            }
+            
 
-    setCanvasSize(yyy);
-
-}
-
-//鼠标点击监听
-document.onmousedown=function(aaa){
-    var x=aaa.clientX;
-    var y=aaa.clientY;
-    lastPoint={x:x,y:y};
-    if(eraserEnabled){
-        using=true;
-        context.clearRect(x-10,y-10,20,20);
-    }else{
-        using=true;
-        drawCir(x,y);
-    }
-}
-
-//鼠标移动监听
-document.onmousemove=function(aaa){
-    var x=aaa.clientX;
-    var y=aaa.clientY;
-    var newPoint={x:x,y:y}
-   
-    if(eraserEnabled){
-        if(using){
-            context.clearRect(x-10,y-10,20,20);
+        }
+        canvas.ontouchend = function(aaa){
+            using=false;
         }
 
     }else{
-        if(using){ 
-            drawCir(x,y);
-            drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
-            lastPoint=newPoint;
+        //不是触屏设备
+        canvas.onmousedown=function(aaa){
+            var x=aaa.clientX;
+            var y=aaa.clientY;
+            using=true;
+            lastPoint={x:x,y:y};
+            if(eraserEnabled){
+                context.clearRect(x-10,y-10,20,20);
+            }else{
+                drawCir(x,y);
+            }
         }
+        
+        //鼠标移动监听
+        canvas.onmousemove=function(aaa){
+            var x=aaa.clientX;
+            var y=aaa.clientY;
+            var newPoint={x:x,y:y}
+            if(using){
+                if(eraserEnabled){
+                    context.clearRect(x-10,y-10,20,20);
+              }else{
+                    drawCir(x,y);
+                    drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+                    lastPoint=newPoint;      
+                }
+            }
+        }
+        
+        //鼠标松开监听
+        canvas.onmouseup=function(aaa){
+            using=false;
+        }
+
     }
-}
+    //鼠标点击监听
 
-//鼠标松开监听
-document.onmouseup=function(aaa){
-    using=false;
-}
-  
- 
-/********/
-  
-var eraserEnabled=false;
-
-eraser.onclick=function(){
-    eraserEnabled = !eraserEnabled;
 }
